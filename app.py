1b235b09eac43018c629971dd7a22885356ef5d9
codersid19 <shirodkar.siddhant19@gmail.com>
2025-01-23 11:35:04 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load dataset
+data = pd.read_csv('data\dataset_house_apartment.csv')
+
+@app.route("/", methods=['GET'])
+def welcome():
+    return "Hellow WOrld"
+
+@app.route('/properties', methods=['GET'])
+def get_properties():
+    location = request.args.get('location')
+    if location:
+        filtered = data[data['location'] == location]
+    else:
+        filtered = data
+    return jsonify(filtered.to_dict(orient='records'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
